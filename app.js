7f67e0754e807277ae386f37dc2ec8bbbf36c252
Robert Schofield <rschofield1992@gmail.com>
2024-04-26 16:03:05 -0400

    commit

--- app.js
+++ app.js
@@ -4,7 +4,34 @@ console.log(
 );
 
 // Exercise 1
+let lastName= "Schofield";
+var age= 31;
 
 // Exercise 2
+let firstName= "Robert";
 
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+// exercise 4
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+//exercise 5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 772;
+let bookmark = 456;
+let hasRead = true;
+
+
+
+
+
+
+
+
